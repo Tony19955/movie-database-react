20f1bc02845f5ea87467fff27b2fc694f8396bbe
matti <mattias.mik@gmail.com>
2023-08-03 17:38:22 +0200

    Deleting .env from Git

--- src/main.tsx
+++ src/main.tsx
@@ -1,20 +1,23 @@
 import './i18n.ts';
 import './styles/reset.css';
 
-import React from 'react';
+import { StrictMode } from 'react';
 import { Provider } from 'react-redux';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 
 import App from './App.tsx';
 import { store } from './redux/store.ts';
+import { ReactErrorBoundary } from './ErrorBoundary.tsx';
 
 ReactDOM.createRoot(document.getElementById('root') as Element).render(
-    <React.StrictMode>
+    <StrictMode>
         <Provider store={store}>
             <BrowserRouter>
-                <App />
+                <ReactErrorBoundary>
+                    <App />
+                </ReactErrorBoundary>
             </BrowserRouter>
         </Provider>
-    </React.StrictMode>
+    </StrictMode>
 );
